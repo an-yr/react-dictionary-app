d96d0c05ecc3669a467e970832258f33acfca3ab
Angela Yuri <ang.saito@gmail.com>
2021-12-20 14:22:02 -0300

    added a search engine

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import Dictionary from "./Dictionary";
 function App() {
   return (
     <div className="App">
-      <Dictionary />
+      <div className="background-purple">
+        <div className="background-second">
+          <div className="background-third">
+            <h1>Dictionary App</h1>
+            <Dictionary />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,54 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Dictionary.css";
 import image_lupa from "./image_lupa.png";
 
 export default function Dictionary() {
+  const [keyword, setKeyword] = useState(null);
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword}`);
+  }
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
   return (
-    <div className="background-purple">
-      <div className="background-second">
-        <div className="background-third">
-          <h1>Dictionary App</h1>
-          <form>
-            <input
-              className="input"
-              type="search"
-              placeholder="Search word..."
-              autoFocus="on"
-            />
-            <button type="submit" value="Search">
-              <i className="fas fa-search"></i>
-            </button>
-          </form>
-          <img
-            className="lupa"
-            src={image_lupa}
-            alt="Two girls on the phone reading"
-          />
-          {/* results */}
-          <div className="results">
-            <div className="result">
-              <h2>Word</h2>
-              <br />
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi,
-              laborum. Unde praesentium, enim necessitatibus soluta sapiente
-              modi quaerat. Totam temporibus et quo aliquam vitae, blanditiis
-              labore. Minima earum laborum corrupti!
-            </div>
-            <div className="result">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor
-              cumque rem molestiae saepe maxime ipsam necessitatibus provident
-              odio aspernatur, a odit laudantium quia architecto libero
-              dignissimos assumenda eaque iure sed!
-            </div>
-            <div className="result">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor
-              cumque rem molestiae saepe maxime ipsam necessitatibus provident
-              odio aspernatur, a odit laudantium quia architecto libero
-              dignissimos assumenda eaque iure sed!
-            </div>
-          </div>
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input
+          className="input"
+          type="search"
+          placeholder="Search word..."
+          autoFocus="on"
+          onChange={handleKeywordChange}
+        />
+        <button type="submit" value="Search">
+          <i className="fas fa-search"></i>
+        </button>
+      </form>
+      <img
+        className="lupa"
+        src={image_lupa}
+        alt="Two girls on the phone reading"
+      />
+      {/* results */}
+      {/* <div className="results">
+        <div className="result">
+          <h2>Word</h2>
+          <br />
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi,
+          laborum. Unde praesentium, enim necessitatibus soluta sapiente modi
+          quaerat. Totam temporibus et quo aliquam vitae, blanditiis labore.
+          Minima earum laborum corrupti!
         </div>
-      </div>
+        <div className="result">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor cumque
+          rem molestiae saepe maxime ipsam necessitatibus provident odio
+          aspernatur, a odit laudantium quia architecto libero dignissimos
+          assumenda eaque iure sed!
+        </div>
+        <div className="result">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor cumque
+          rem molestiae saepe maxime ipsam necessitatibus provident odio
+          aspernatur, a odit laudantium quia architecto libero dignissimos
+          assumenda eaque iure sed!
+        </div>
+      </div> */}
     </div>
   );
 }
