9aa1365cc17e4846b0bb10a1cdadbbdac28651df
Angela Yuri <ang.saito@gmail.com>
2021-12-23 11:14:04 -0300

    added a link to GitHub repository

--- src/App.js
+++ src/App.js
@@ -9,6 +9,14 @@ function App() {
           <div className="background-third">
             <h1>Dictionary App</h1>
             <Dictionary />
+            <p>
+              <strong>
+                Coded by Angela Yuri and{" "}
+                <a href="https://github.com/an-yr/react-dictionary-app">
+                  open-sourced
+                </a>
+              </strong>
+            </p>
           </div>
         </div>
       </div>
