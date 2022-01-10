61fad379dcaf8308234de1dec4166dbfa3cda463
Angela Yuri <ang.saito@gmail.com>
2022-01-10 13:03:11 -0300

    fixed the error

--- src/Giphy.js
+++ src/Giphy.js
@@ -1,21 +1,25 @@
 import React from "react";
 
 export default function Giphy(props) {
-  return (
-    <div className="Giphy">
-      <div className="row">
-        {props.giphy.data.map(function (giphy, idx) {
-          return (
-            <div className="col-4" key={idx}>
-              <img
-                src={giphy.images.fixed_height.url}
-                alt="gifs"
-                className=""
-              ></img>
-            </div>
-          );
-        })}
+  if (props.giphy) {
+    return (
+      <div className="Giphy">
+        <div className="row">
+          {props.giphy.data.map(function (giphy, idx) {
+            return (
+              <div className="col-4" key={idx}>
+                <img
+                  src={giphy.images.fixed_height.url}
+                  alt="gifs"
+                  className=""
+                ></img>
+              </div>
+            );
+          })}
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return null;
+  }
 }
